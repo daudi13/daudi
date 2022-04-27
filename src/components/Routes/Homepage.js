884abead99106ceb9e8f1a1526c480ba580f5924
daudi13 <davouma13@gmail.com>
2022-04-27 15:54:39 +0300

    Add about me section

--- src/components/Routes/Homepage.js
+++ src/components/Routes/Homepage.js
@@ -1,19 +1,47 @@
-import Header from '../Header';
 import './Homepage.css';
+import myPic from '../images/20200217_111546(0).jpg'
 
 function Homepage() {
   return (
     <div className="App">
-      <section class="intro" id="home!">
-            <div class="align-left" data-aos="zoom-in">
-                <p class="hello">Hello, I am David,</p>
-                <h2 class="job-des">I'm a <br/> <span class="special ">Front-end Developer</span></h2>
-                <p class="belief">
+      <section className="intro" id="home">
+            <div className="align-left" data-aos="zoom-in">
+                <h1 className="hello">Hello, I'm David,</h1>
+                <h2 className="job-des">A <span className="special ">Front-end Developer</span></h2>
+                <p className="belief">
                     "I believe in diverse skill sets and people as they make products and communities unique. As a developer I believe in changing the world using technology and improving individuals to be better versions of themselves by supporting small businesses and community based organizations with an aim of making the blue planet a better place for each and everyone of us."
                 </p>
-                <a href="#about!" class="btn">Read more</a>
             </div>
-    </section>
+      </section>
+      <section className="about">
+        <div className="about-title"><h2>About Me</h2><span><hr className="line"/></span></div>
+        <div className="desc">
+          <div className="wording">
+        <p className="belief">
+          Currently a I'm a front-end developer who enjoys bringing ideas to life by translating web designs and wireframes to functional websites. I'm also an effective developer who brings out the vision and executes the strategy with minimal resources
+        </p>
+        <p className="belief">
+          In my 3 years of experience in web development, I've developed extensive knowledge in :
+        </p>
+        <ul className="list">
+          <li> Front-end development: ReactJS | Vanilla Javascript| API | Apollo Client</li>
+          <li>Styling: Styled-components | SASS | CSS-in-JS | Bootstrap </li>
+          <li>Pair programming and remote working</li>
+          <li>Version Control: GIT | Github</li>
+          <li>Product management: Figma | wireframing</li>
+          <li>Hosting: Heroku | Githubpages | Vercel | Netlify </li>
+        </ul>
+        <p className="belief">
+          I'm passionate about sharing knowledge, implementing product designs and solving problems using front-end technologies. I'm also a team-player and I'm always looking forward to join a passionate team of developers. At the moment I'm open to Front-end opportunities in small start-ups as well a big companies.
+            </p>
+          </div>
+          <img src={myPic} className="pix" alt="David ouma"/>
+          </div>
+      </section>
+      <section className="experience">
+      </section>
+      <section className="testimonials">
+      </section>
     </div>
   );
 }
