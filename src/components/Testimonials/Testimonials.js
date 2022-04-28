2c5db16a8c68b01606bd7de138642b0886a313f4
daudi13 <davouma13@gmail.com>
2022-04-28 19:40:00 +0300

    Update testimonial.js

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -17,41 +17,42 @@ const Testimonials = () => {
       autoPlay={true}
       interval={6100}
     >
-      <div className="carosel-wrapper">
-        <div className="myCarousel">
-          <img src={souad} alt='souad' />
-          <h3 className='name'>Souad El Mansouri</h3>
-          <h4 className="job">Full-stack developer</h4>
-          <p className="desc">
-            David is a great developer and coding partner. I had an opportunity to build a project with Him through remote paired programming and the experience was one of the best. As a team player, he helps when requested and participates in all group activities making sure the goal of the team is achieved.
-          </p>
+        <div>
+          <img src={emmanuel} alt="emmauel" />
+          <div className="myCarousel">
+            <h3>Shirley Fultz</h3>
+            <h4>Designer</h4>
+            <p>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
         </div>
-        <div className="myCarousel">
-          <img src={emmanuel} alt='Emmanuel Jolugbo' />
-          <h3 className='name'>Emmanuel Jolugboi</h3>
-          <h4 className="job">Software Engineer</h4>
-          <p>
-            David is a fantastic software engineer, and his detail-oriented approach made him a pleasure to work with. We pair-programmed several times together while enrolled at Microverse a software development program, and during that time his work ethic blew me away. David views writing clean, accessible code as a calling, and he's great at identifying areas where we can improve UI.
-          </p>
+
+        <div>
+          <img src={souad} alt="emma"/>
+          <div className="myCarousel">
+            <h3>Daniel Keystone</h3>
+            <h4>Designer</h4>
+            <p>
+              The simple and intuitive design makes it easy for me use. I highly
+              recommend Fetch to my peers.
+            </p>
+          </div>
         </div>
-        <div className="myCarousel">
-          <img src={nemwel} alt='Nemwel Nyandoro' />
-          <h3 className='name'>Nemwel Nyandoro</h3>
-          <h4 className="job">Front-end Developer</h4>
-          <p className="desc">
-            ...David played the role of the team leader in a project that we worked on while students at Microverse and aside from his technical skills his people skills shined. He is an empathetic team leader who understands that things are not always easy for everyone and he uses his communication skills to encourage you and helps you come to par with the rest of the team.
-          </p>
+
+        <div>
+          <img src={nemwel} alt="emma" />
+          <div className="myCarousel">
+            <h3>Theo Sorel</h3>
+            <h4>Designer</h4>
+            <p>
+              I enjoy catching up with Fetch on my laptop, or on my phone when
+              I'm on the go!
+            </p>
+          </div>
         </div>
-        <div className="myCarousel">
-          <img src={soufiane} alt='Soufiane Boursen' />
-          <h3 className='name'>Soufiane Boursen</h3>
-          <h4 className="job">Full-stack developer</h4>
-          <p className="desc">
-            When I think of David, the first thing that comes to mind is Solid Foundations. He is always striving to use the best practices in his websites, writing good quality code all while meeting deadlines. He is a diligent programmer, with a keen eye for details, and a goal-oriented mindset
-          </p>
-        </div>
-      </div>
-    </Carousel>
+      </Carousel>
   )
 }
 
