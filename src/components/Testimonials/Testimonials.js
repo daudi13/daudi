4e607b1bfb175288cac942344a79e6c47fabc3e7
daudi13 <davouma13@gmail.com>
2022-04-28 17:39:59 +0300

    Add profile pics

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -4,7 +4,11 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <Carousel>
+      <div>
+        <img src="https://ca.slack-edge.com/T47CT8XPG-U02KAEKCW0G-45576989ed08-72" alt='souad'/>
+      </div>
+    </Carousel>
   )
 }
 
