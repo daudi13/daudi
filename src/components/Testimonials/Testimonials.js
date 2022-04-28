04307225ca974f84da5023c360dd8604c61bf1f5
daudi13 <davouma13@gmail.com>
2022-04-28 18:43:33 +0300

    Add images

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -20,19 +20,22 @@ const Testimonials = () => {
         <div className="myCarousel">
           <img src={souad} alt='souad' />
           <h3 className='name'>Souad El Mansouri</h3>
-
+          <h4 className="job">Full-stack developer</h4>
         </div>
         <div className="myCarousel">
-          <img src={emmanuel} alt='souad' />
-          <h3 className='name'>Souad El Mansouri</h3>
+          <img src={emmanuel} alt='Emmanuel Jolugbo' />
+          <h3 className='name'>Emmanuel Jolugboi</h3>
+          <h4 className="job">Software Engineer</h4>
         </div>
         <div className="myCarousel">
-          <img src={nemwel} alt='souad' />
-          <h3 className='name'>Souad El Mansouri</h3>
+          <img src={nemwel} alt='Nemwel Nyandoro' />
+          <h3 className='name'>Nemwel Nyandoro</h3>
+          <h4 className="job">Front-end Developer</h4>
         </div>
         <div className="myCarousel">
-          <img src={soufiane} alt='souad' />
-          <h3 className='name'>Souad El Mansouri</h3>
+          <img src={soufiane} alt='Soufiane Boursen' />
+          <h3 className='name'>Soufiane Boursen</h3>
+          <h4 className="job">Full-stack developer</h4>
         </div>
       </div>
     </Carousel>
