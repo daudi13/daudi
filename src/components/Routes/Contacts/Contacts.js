f5a7f729fb7756ba49045bad31558946746b60e8
daudi13 <davouma13@gmail.com>
2022-04-29 02:42:57 +0300

    Add contact intro

--- src/components/Routes/Contacts/Contacts.js
+++ src/components/Routes/Contacts/Contacts.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <section className="contact-form-section">
+      <div className="contact-form__container">
+        <div className="outro__box">
+          <h2 className="headline-section__title white">Contact me</h2>
+          <p className="headline-section__desc white">If you have an application you are interested in developing, a feature that you need built or a project that needs coding. I’d love to help with it </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
