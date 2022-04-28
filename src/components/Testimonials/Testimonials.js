c5ec53f598a354201779b9645d712c1abaee2d3c
daudi13 <davouma13@gmail.com>
2022-04-28 17:48:16 +0300

    create rough copy testimonials

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import emmanuel from '../images/emmanuel.jpeg';
+import nemwel from '../images/nemwel.jpeg';
+import souad from '../images/souad.jpeg';
+import soufiane from '../images/soufiane.jpeg';
 
 const Testimonials = () => {
   return (
     <Carousel>
       <div>
-        <img src="https://ca.slack-edge.com/T47CT8XPG-U02KAEKCW0G-45576989ed08-72" alt='souad'/>
+        <img src={souad} alt='souad' />
+        <p className='legend'>Software Developer</p>
+      </div>
+      <div>
+        <img src={emmanuel} alt='souad' />
+        <p className='legend'>Software Developer</p>
+      </div>
+      <div>
+        <img src={nemwel} alt='souad' />
+        <p className='legend'>Software Developer</p>
+      </div>
+      <div>
+        <img src={soufiane} alt='souad' />
+        <p className='legend'>Software Developer</p>
       </div>
     </Carousel>
   )
