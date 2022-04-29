bc283217347dc930ea9604849883ed180cab459e
daudi13 <davouma13@gmail.com>
2022-04-30 00:09:55 +0300

    Add perspective wild

--- src/components/DataBase/DataBase.js
+++ src/components/DataBase/DataBase.js
@@ -1,12 +1,14 @@
 
 const modals = [{
   modalId: 'con-1',
-  name: 'Tonic',
-  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and .",
+  name: 'PerspectiveWild',
+  desc: "This is a react project that showcase the different types of animals both wild and domesticated. The project aims to shade light on the need to preserved the different species of animals and at the same time share facts curated for specific animals species. ",
   order: 'order--one',
   imageOneDesktop: './images/Card-images/SnapshootPortfolioDesktop1.png',
-  skillSet: ['html', 'css', 'javaScript'],
-  jobProfile: ['CANOPY', 'Back End Dev', 2018],
+  skillSet: ['React', 'Jest', 'Thunk', 'bootstrap', 'redux'],
+  jobProfile: ['FREELANCER', 'Front-End Dev', 2022],
+  liveDemo:'https://6262b89a578849338355be2c--benevolent-peony-90404f.netlify.app/',
+  gitHub:'https://github.com/daudi13/seafood',
 },
 {
   modalId: 'con-2',
